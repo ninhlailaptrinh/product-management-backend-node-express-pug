4ac269112481e6773a8298409a5cb7b419002c45
ninhlailaptrinh <ninhlailaptrinh@gmail.com>
2025-01-03 15:39:59 +0700

    Hoàn thiện tính năng bộ lọc

--- product-management/controllers/admin/product.controller.js
+++ product-management/controllers/admin/product.controller.js
@@ -1,14 +1,50 @@
 const Product = require("../../models/product.model");
 
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
-    deleted: false,
-  });
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
 
-  console.log(products);
+  if (req.query.status) {
+    const index = filterStatus.findIndex(
+      (item) => item.status == req.query.status,
+    );
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex((item) => item.status == "");
+    filterStatus[index].class = "active";
+  }
+  console.log(req.query.status);
+
+  let find = {
+    deleted: false,
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const products = await Product.find(find);
+
+  //console.log(products);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
+    filterStatus: filterStatus,
   });
 };
